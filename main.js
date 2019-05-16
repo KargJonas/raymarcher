f2dc27049d287a8841595a61fde7ff5b321aa23e
Jonas Karg <jonas.karg@hotmail.com>
2019-05-16 19:09:36 +0200

    Trying to do all painting with GLSL

--- main.js
+++ main.js
@@ -1,33 +1,12 @@
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const cnv = document.querySelector("canvas")
+const gl = cnv.getContext("webgl")
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+if (!gl) throw new Error("No webgl support")
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-// const material = new THREE.MeshNormalMaterial();
-const material = new THREE.MeshNormalMaterial({color: "#ff0000"});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const vertices = [
+  -1, -1, 0,
+  1, -1, 0,
+  1, 1, 0,
+  -1, 1, 0,
+];
 
-camera.position.z = 5;
-
-let mouseX = 0;
-let mouseY = 0;
-
-window.addEventListener("mousemove", (e) => {
-  mouseX = e.clientX;
-  mouseY = e.clientY;
-})
-
-function animate() {
-  requestAnimationFrame(animate);
-  // cube.rotateY(0.01);
-  // cube.rotateX(0.01);
-  cube.rotation.y = (mouseX / window.innerWidth) * 3;
-  cube.rotation.x = (mouseY / window.innerHeight) * 3;
-  renderer.render(scene, camera);
-}
-
-animate();
